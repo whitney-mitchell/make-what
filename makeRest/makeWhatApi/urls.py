ca8adb34d344261e5791eeaf5891ed3204c7e98a
whitney-mitchell <whitney.mitchell84@gmail.com>
2016-09-14 12:55:44 -0500

    forgot to define router before last commit. Whoops.

--- makeRest/makeWhatApi/urls.py
+++ makeRest/makeWhatApi/urls.py
@@ -3,7 +3,10 @@ from django.conf.urls import url, include
 from makeWhatApi import views
 
 # The URL pattern that the project calls, now points to index.
-app_name = "makeWhatApi"
+# app_name = "makeWhatApi"
+
+router = routers.DefaultRouter()
+
 urlpatterns = [
 	url(r'^register/$', views.register_user, name='register_user'),
 	url(r'^login/$', views.login_user, name='login_user'),
